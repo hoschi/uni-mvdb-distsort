9bf1ca95fe379692c2a56a5c3fa88a13a46690f2
Stefan Gojan <vollekannehoschi@gmail.com>
2010-11-08 13:30:18 +0100

    [#6018603] added test for 'add' method

---
+++ sortserver/src/main/java/de/uniluebeck/ifis/mvdbproject/Server.java
@@ -0,0 +1,14 @@
+package de.uniluebeck.ifis.mvdbproject;
+
+import java.util.List;
+
+public class Server {
+	public void add(String s) {
+		// TODO implement it!
+	}
+
+	public List<String> getList() {
+		// TODO implement it!
+		return null;
+	}
+}
--- sortserver/src/test/java/de/uniluebeck/ifis/mvdbproject/SortServer.java
+++ sortserver/src/test/java/de/uniluebeck/ifis/mvdbproject/SortServer.java
@@ -4,16 +4,30 @@ import org.junit.*;
 import java.util.List;
 import java.util.ArrayList;
 
+import de.uniluebeck.ifis.mvdbproject.Server;
+
 public class SortServer {
-	List<String> unsortet;
+	List<String> unsorted;
 
 	@Before
 	public void setUp() throws Exception {
-		this.unsortet = new ArrayList<String>();
-		this.unsortet.add("a");
-		this.unsortet.add("d");
-		this.unsortet.add("c");
-		this.unsortet.add("b");
+		this.unsorted = new ArrayList<String>();
+		this.unsorted.add("a");
+		this.unsorted.add("d");
+		this.unsorted.add("c");
+		this.unsorted.add("b");
+	}
+
+	@Test
+	public void testAddMethod() {
+		Server server = new Server();
+		for (String s : this.unsorted) {
+			server.add(s);
+		}
+		
+		List<String> list = server.getList();
+		Assert.assertArrayEquals("server list and local list aren't equal",
+				this.unsorted.toArray(), list.toArray());
 	}
 
 }
