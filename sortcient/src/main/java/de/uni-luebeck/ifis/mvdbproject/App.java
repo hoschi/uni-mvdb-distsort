133604302a92ee3202c42c4ffa2330554c350993
Stefan Gojan <vollekannehoschi@gmail.com>
2010-11-08 11:14:57 +0100

    initial

---
+++ sortcient/src/main/java/de/uni-luebeck/ifis/mvdbproject/App.java
@@ -0,0 +1,13 @@
+package de.uni-luebeck.ifis.mvdbproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
